2d4735a560d211487cdb0da00dd52a65786d8472
Johyan <65616991+JohyanC@users.noreply.github.com>
2024-03-18 21:13:02 +0000

    Creates initial files (mostly blank)

---
+++ iventoryPrimary.cpp
@@ -0,0 +1,16 @@
+/*C++ program to manage an inventory system*/
+#include <iostream>
+#include <vector>
+#include <database.h>
+
+using namespace std;
+
+int main() {
+    int hbSize = 5;
+
+    //Creates a vector of hbSize for the player hotbar
+    vector<string> hotbar(hbSize);  
+
+    
+    return 0;
+}
